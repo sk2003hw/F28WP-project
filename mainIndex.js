c3ec1c126506584ec83eb7150daa96e511cc9593
apvaccess <71452462+apvaccess@users.noreply.github.com>
2020-10-18 18:56:39 +0400

    Create mainIndex.js

---
+++ mainIndex.js
@@ -0,0 +1,30 @@
+/*
+  This File Will Animate The "Begin Game" Button.
+  All user details collected and user information are sent as cookies to the main "game.js" JS File.
+
+*/
+
+$(document).ready(function(){ 
+    
+    //CANVAS SELECTOR FEATURE
+ var canvas = $('#myCanvas');
+
+   //ON LOADING - The div will be of color black and will prompt user to begin game.
+ canvas.css('background-color', 'black');
+
+   //The 'Begin Game' text will grow on hover and the user clicks to begin the game.
+ $('#begin').hover(
+     function(){
+         $(this).animate({fontSize: '105px'}, 1000);
+     },        
+     function(){
+         $(this).animate({fontSize: '75px'}, 1000);
+     },
+ );
+
+ var socket = io();
+   //On Sign up, the user details will be sent as COOKIES.
+ $('#signup').submit(function () {
+     socket.emit('user_details', $('#username').val(), $('#password').val());
+ });    
+});
