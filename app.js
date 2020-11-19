3bf64b4102536744d884e5999d697a61c73f4fb0
Hammad2002 <71422360+Hammad2002@users.noreply.github.com>
2020-11-19 07:57:56 +0400

    .

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
 /* this file is used for the server part of the game*/
 const express = require('express');
-const dataparser = require('body-parser');
-var crypto = require('crypto');
 const app = express();
-const urlencodedParser = dataparser();
-var server = require('http').Server(app);
 var username;
 var password;
+const http = require('http');
+const server = http.createServer((request, response) => {
+    if (request.url === '/') {
+        response.write("Hello");
+        response.write("Restarting server");
+        response.end();}});
 
-app.use(express.static(__dirname + '/client'));
-app.get('/' , function(req,res) {
-    res.sendFile(__dirname + '/client/index.html');
-    console.log(__dirname + '/client/index.html');
-});
-var mysql      = require('mysql');                             
-var connection = mysql.createConnection({                       
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'players',
-});
+app.use(express.static('client'));
+app.get('/' , function(request,response) {
+    response.sendFile({root: __dirname} + '/client/index.html')});
+
