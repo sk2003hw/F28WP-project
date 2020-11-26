3b2cfacc6d2fb756bdbf9daaf11da78651e2a452
sk2003hw <71402153+sk2003hw@users.noreply.github.com>
2020-11-26 09:18:11 +0300

    Update game.js

--- client/game.js
+++ client/game.js
@@ -1,5 +1,23 @@
 $(document).ready(function(){
 
+    /*Code tried for user
+    var getUrlParameter = function getUrlParameter(sParam) {
+        var sPageURL = window.location.search.substring(1),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+            }
+        }
+    };
+    var user = getUrlParameter("userId");
+    
+    $('#racer').text(user);
+    alert("User "+user);
+    */
     var gamecourse = $('#playArea');                            //Referring to the area where we play the game
     gamecourse.addClass('animate-area');                        //To animate game canvas 
     var racer = $('#racer');                                    //Selector for racer
