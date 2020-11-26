9975a92ce19915a4fea1b796bda7e5564fa64854
sk2003hw <71402153+sk2003hw@users.noreply.github.com>
2020-11-26 08:19:53 +0300

    attempt of leaderboard

--- client/scores.js
+++ client/scores.js
@@ -1,7 +1,7 @@
-$(document).ready(function(){
-    $.get('/api/scores',function(scores) {
-            for(var i = 0; i<scores.length; i++){
-                $('#scores').append($('<li>').html(scores[i].username+ ":" + scores[i].score ));
-            }
-    });
-});
+//$(document).ready(function(){
+  //  $.get('/api/scores',function(scores) {
+    //        for(var i = 0; i<scores.length; i++){
+             //   $('#scores').append($('<li>').html(scores[i].username+ ":" + scores[i].score ));
+      //      }
+    //});
+//});
